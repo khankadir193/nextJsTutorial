eaafb7aa02e075b00327e1a9d533fc2c4f816cab
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-03-29 22:01:46 +0530

    link page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
   const [name,setName] = useState('Abdullah Khan');
@@ -19,6 +20,9 @@ export default function Home() {
       <h1>Hello Next.js</h1>
       <h2>Hi What is your name:{name}</h2>
       <button onClick={handleClick}>click</button>
+
+      <Link href="/login">Go to login Page</Link>
+      <Link href="/about">Go to about Page</Link>
     </main>
   );
 }
